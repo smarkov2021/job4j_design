b4ed9070c4a5dea69c8b527a206bfc0f213afba7
smarkov <markovsv777@mail.ru>
2023-02-23 20:13:58 +0300

    ForwardLinked

---
+++ src/main/java/ru/job4j/collection/ForwardLinked.java
@@ -0,0 +1,90 @@
+package ru.job4j.collection;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class ForwardLinked<T> implements Iterable<T> {
+    private int size = 0;
+    private int modCount = 0;
+    private Node<T> head;
+
+    public void add(T value) {
+        Node<T> l = head;
+        final Node<T> newNode = new Node<>(value, null);
+        if (head == null) {
+            head = newNode;
+        } else {
+            while (l.next != null) {
+                l = l.next;
+            }
+            l.next = newNode;
+        }
+        size++;
+        modCount++;
+    }
+
+    public T get(int index) {
+        Objects.checkIndex(index, size);
+        Node<T> param = head;
+        for (int i = 0; i < index; i++) {
+            param = param.next;
+        }
+        return param.getItem();
+    }
+
+    public T deleteFirst() {
+        if (head != null) {
+            Node newHead = head.next;
+            head.next = null;
+            head.item = null;
+            head = newHead;
+        } else {
+            throw new NoSuchElementException();
+        }
+        return head.item;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        int expectedModCount = modCount;
+        return new Iterator<T>() {
+            Node<T> param = head;
+            Node<T> prev;
+
+            @Override
+            public boolean hasNext() {
+                if (modCount != expectedModCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return param != null;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                prev = param;
+                param = param.next;
+                return prev.getItem();
+            }
+        };
+    }
+
+    private static class Node<T> {
+        private T item;
+
+        public T getItem() {
+            return item;
+        }
+
+        private Node<T> next;
+
+        Node(T element, Node<T> next) {
+            this.item = element;
+            this.next = next;
+        }
+    }
+}
