1425ed9d4d33e81468781199108a97c779d8dd54
smarkov <markovsv777@mail.ru>
2023-02-24 11:25:06 +0300

    ForwardLinked-Stack

--- src/main/java/ru/job4j/collection/ForwardLinked.java
+++ src/main/java/ru/job4j/collection/ForwardLinked.java
@@ -35,7 +35,7 @@ public class ForwardLinked<T> implements Iterable<T> {
     }
 
     public void addFirst(T value) {
-        head = new Node(value,  head);
+        head = new Node(value, head);
     }
 
     public T deleteFirst() {
