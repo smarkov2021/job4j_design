25752fd52f734f047b2cc4eff50b8448de32fb76
smarkov <markovsv777@mail.ru>
2023-07-07 18:19:16 +0300

    Search

---
+++ src/main/java/ru/job4j/io/ByteArrayStream.java
@@ -0,0 +1,25 @@
+package ru.job4j.io;
+
+import java.io.*;
+
+public class ByteArrayStream {
+
+    public static void main(String[] args) {
+        byte[] bytes = new byte[] {'J', 'a', 'v', 'a'};
+        ByteArrayInputStream stream = new ByteArrayInputStream(bytes);
+        int data;
+        while ((data = stream.read()) != -1) {
+            System.out.print((char) data);
+        }
+        System.out.println();
+        ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+        byte[] bytes2 = "Message".getBytes();
+        outStream.writeBytes(bytes2);
+        System.out.println(outStream);
+        try (FileOutputStream fileOutput = new FileOutputStream("data/message.txt")) {
+            outStream.writeTo(fileOutput);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
