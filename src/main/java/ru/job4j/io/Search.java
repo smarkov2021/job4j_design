b811bf1fe5b83f0c48cc64de7a6b212e6a29374b
smarkov <markovsv777@mail.ru>
2023-07-09 20:47:28 +0300

    Search

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -8,14 +8,24 @@ import java.util.List;
 import java.util.function.Predicate;
 
 public class Search {
-    public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith(".js")).forEach(System.out::println);
-    }
-
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    public static void validate(String[] args) {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Root folder is null. Usage  ROOT_FOLDER.");
+        }
+        if (args.length == 1) {
+            throw new IllegalArgumentException("Filename extension is null. Usage  FILE_EXTENSION.");
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        validate(args);
+        Path start = Paths.get(args[0]);
+        search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 }
