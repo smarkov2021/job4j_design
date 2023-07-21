d12edbd8b23a66c2e5d2529e46c9c77b29ffc718
smarkov <markovsv777@mail.ru>
2023-07-21 16:10:24 +0300

    ConsoleChat

---
+++ src/main/java/ru/job4j/io/ConsoleChat.java
@@ -0,0 +1,71 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class ConsoleChat {
+    private static final String OUT = "закончить";
+    private static final String STOP = "стоп";
+    private static final String CONTINUE = "продолжить";
+    private final String path;
+    private final String botAnswers;
+
+    public ConsoleChat(String path, String botAnswers) {
+        this.path = path;
+        this.botAnswers = botAnswers;
+    }
+
+    public void run() {
+        List<String> answers = readPhrases();
+        List<String> log = new ArrayList<>();
+        Random rand = new Random();
+        Scanner in = new Scanner(System.in);
+        String ask;
+        boolean isStop = false;
+        do {
+            ask = in.nextLine();
+            log.add(ask);
+            if (ask.equals(STOP))  {
+                isStop = true;
+            } else if (ask.equals(CONTINUE) && isStop) {
+                String answer = answers.get(rand.nextInt(answers.size()));
+                log.add(answer);
+                System.out.println(answer);
+                isStop = false;
+            } else if (!isStop && !ask.equals(OUT)) {
+                String answer = answers.get(rand.nextInt(answers.size()));
+                log.add(answer);
+                System.out.println(answer);
+            }
+        } while (!ask.equals(OUT));
+        saveLog(log);
+    }
+
+    private List<String> readPhrases() {
+        List<String> rsl = null;
+        try (BufferedReader br = new BufferedReader(new FileReader(botAnswers))) {
+            rsl = br.lines().toList();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return rsl;
+    }
+
+    private void saveLog(List<String> log) {
+        try (PrintWriter pw = new PrintWriter(new FileWriter(path,
+                Charset.forName("WINDOWS-1251"), true))) {
+            log.forEach(pw::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        ConsoleChat cc = new ConsoleChat("./log.txt", "./text.txt");
+        cc.run();
+    }
+}
