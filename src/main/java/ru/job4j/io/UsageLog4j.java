632864e867bbb2d3a53e55b65523f215b3bee4a7
smarkov <markovsv777@mail.ru>
2023-07-30 13:18:27 +0300

    Log4j

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,10 +8,20 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        String name = "Sergey";
+        int age = 33;
+        byte id = 3;
+        short home = 435;
+        int sum = 65235;
+        long uid = 33345643;
+        double weight = 90.2;
+        boolean child  = false;
+        String street = "Kuibyshev";
+        char liter = 'A';
+        float contribution = 7.43F;
+        LOG.debug("User info name : {}, age : {}", name, age);
+        LOG.debug("User address. Street : {}, home : {}, liter : {}", street, home, liter);
+        LOG.debug("Users addition information. weight : {}, id : {}, uid : {}, sum : {}, contribution : {}, child : {}",
+                weight, id,  uid, sum, contribution, child);
     }
 }
