967a4cf2994d4a7e79c45a4d0ed921a2f19e5d5a
smarkov <markovsv777@mail.ru>
2023-08-06 20:51:00 +0300

    JSON

--- src/main/java/ru/job4j/serialization/json/Person.java
+++ src/main/java/ru/job4j/serialization/json/Person.java
@@ -1,6 +1,11 @@
 package ru.job4j.serialization.json;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Person {
     private final boolean sex;
@@ -23,5 +28,34 @@ public class Person {
                 + ", contact=" + contact
                 + ", statuses=" + Arrays.toString(statuses)
                 + '}';
+    }
+
+    public boolean getSex() {
+        return sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public static void main(String[] args) {
+
+        JSONObject jsonContact = new JSONObject("{\"phone\":\"+7(924)111-111-11-11\"}");
+
+        List<String> list = new ArrayList<>();
+        list.add("Student");
+        list.add("Free");
+        JSONArray jsonStatuses = new JSONArray(list);
+
+        final Person person = new Person(false, 30, new Contact("11-111"),
+                new String[] {"Worker", "Married"});
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("sex", person.getSex());
+        jsonObject.put("age", person.getAge());
+        jsonObject.put("contact", jsonContact);
+        jsonObject.put("statuses", jsonStatuses);
+
+        System.out.println(jsonObject.toString());
+        System.out.println(new JSONObject(person).toString());
     }
 }
